61474a3df2e9c1a46a5de8d6d82a02695a80358a
weichen1357 <148304538+weichen1357@users.noreply.github.com>
2024-05-26 01:51:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -161,8 +161,9 @@ def generate_anime_event_carousel(events_info):
                         layout='vertical',
                         margin='lg',
                         contents=[
-                            TextComponent(text=f'活動日期: {event_info["time"]}', size='md'),
-                            TextComponent(text='點我了解更多喲!', size='md', color='#0084B6', align='center' ,action=URIAction(uri=event_info['learn_more_link'], label='了解更多'))
+                            TextComponent(text=f'活動日期: {event_info["time"]}\n', size='md'),
+                            SeparatorComponent(color='#CCCCCC'),
+                            TextComponent(text='點我了解更多唷!', size='md', color='#0084B6', align='center' ,action=URIAction(uri=event_info['learn_more_link'], label='了解更多'))
                         ]
                     )
                 ]
